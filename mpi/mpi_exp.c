3c048338f5d6f0de1d9f79bdaf6e0d5a99ad3b83
pwener <phwener@gmail.com>
2017-04-27 12:35:08 -0300

    Extract methods

--- mpi/mpi_exp.c
+++ mpi/mpi_exp.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <math.h>
 
-#define VECTOR_LEN 1000000000
+#define VECTOR_LEN 1000
 
 #define MAIN_TASK_ID 0
 #define FIRST_NOMAIN_TASK_ID 1
@@ -21,36 +21,38 @@ int fill_vector(float v[VECTOR_LEN]) {
 	}
 }
 
-void assign_chunks(float v[], int chunk_size, int task_numbers) {
+void assign_chunks(float *v, int chunk_size, int task_numbers) {
 	// Attribute chunks of work to each tasks
 	int offset = chunk_size;
 
 	int assigned_task_id;
 	for(assigned_task_id = FIRST_NOMAIN_TASK_ID; assigned_task_id < task_numbers; assigned_task_id++) {
 		if(offset < VECTOR_LEN) {
-			MPI_Send(&v[offset], chunk_size, MPI_FLOAT, assigned_task_id, TAG1, MPI_COMM_WORLD);
+			MPI_Send(&v[offset], chunk_size, MPI_FLOAT, assigned_task_id, TAG2, MPI_COMM_WORLD);
 			offset = offset + chunk_size;
+			printf("Sent %d elements to task %d\n", chunk_size, assigned_task_id);
 		} else {
 			printf("Invalid offset, bigger than array size");
 		}
 	}
 }
 
-void block_and_receive(float v[], int chunk_size, int task_numbers) {
+void block_and_receive(float *v, int chunk_size, int task_numbers) {
 	// Needed for MPI_Recv bellow
 	MPI_Status status;
 
-	int offset;
+	// reuse variable
+	int offset = chunk_size;
 
 	// Waits each task to receive result
 	int source_task_id;
 	for(source_task_id = FIRST_NOMAIN_TASK_ID; source_task_id < task_numbers; source_task_id++) {
-		offset = source_task_id * chunk_size;
-		MPI_Recv(&v[offset], chunk_size, MPI_FLOAT, source_task_id, TAG1, MPI_COMM_WORLD, &status);
+		MPI_Recv(&v[offset], chunk_size, MPI_FLOAT, source_task_id, TAG2, MPI_COMM_WORLD, &status);
+		offset = offset + chunk_size;
 	}
 }
 
-float get_bigger(float v[], int offset, int chunk_size) {
+float get_bigger(float *v, int offset, int chunk_size) {
 		float bigger = 0;
 
 		int index;
@@ -82,7 +84,6 @@ float get_smaller(float *v, int offset, int chunk_size) {
 	return smaller;
 }
 
-
 int main(int argc, char* argv[]) {
 	// declare vector
 	float v[VECTOR_LEN];
@@ -92,6 +93,9 @@ int main(int argc, char* argv[]) {
 	// Get the number of processes
 	int task_numbers;
 	MPI_Comm_size(MPI_COMM_WORLD, &task_numbers);
+
+	// Needed for MPI_Recv bellow
+	MPI_Status status;
 
 	/** Commom variables **/
 	float task_bigger_value, real_bigger_value;
@@ -107,15 +111,20 @@ int main(int argc, char* argv[]) {
 
 	// Main task
 	if (task_id == MAIN_TASK_ID) {
+		printf("Into main task\n");
 
 		fill_vector(v);
 
 		assign_chunks(v, chunk_size, task_numbers);
 
 		block_and_receive(v, chunk_size, task_numbers);
-	} else if (task_id > MAIN_TASK_ID) {
-		MPI_Status status;
 
+		MPI_Reduce(&task_bigger_value, &real_bigger_value, 1, MPI_INT, MPI_MAX, MAIN_TASK_ID, MPI_COMM_WORLD);
+		MPI_Reduce(&task_smaller_value, &real_smaller_value, 1, MPI_INT, MPI_MIN, MAIN_TASK_ID, MPI_COMM_WORLD);
+
+		printf("The bigger found is %f\n", task_bigger_value);
+		printf("The smaller found is %f\n", task_smaller_value);
+	} else if (task_id > MAIN_TASK_ID) {
 		// offset of task
 		int offset = task_id * chunk_size;
 
